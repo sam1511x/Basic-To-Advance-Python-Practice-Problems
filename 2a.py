d06db90dad22489d9ed7cbb1b95f3d3163bd5cfe
Sangam <sangamcodes@gmail.com>
2025-08-01 00:18:40 +0530

    Python program to do arithmetical operations addition

---
+++ 2a.py
@@ -0,0 +1,6 @@
+# Python program to do arithmetical operations addition
+# Addition
+num1 = float(input("Enter the first number for addition: "))
+num2 = float(input("Enter the second number for addition: "))
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result}")
