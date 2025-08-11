67f3a5339e68e849176311ba1678ee94f8ce1f99
Sangam <sangamcodes@gmail.com>
2025-08-11 15:33:49 +0530

    Python program to swap two variables without temp variable

---
+++ 10.py
@@ -0,0 +1,8 @@
+# Python program to swap two variables without temp variable
+a = 5
+b = 10
+# Swapping without a temporary variable
+a, b = b, a
+print("After swapping:")
+print("a =", a)
+print("b =", b)
